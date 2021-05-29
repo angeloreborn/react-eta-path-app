ac83456b01fc912b3bf7b203976646fe947d7167
angeloreborn <angelowolffreborn@gmail.com>
2021-05-29 16:44:45 +0200

    Create Index.tsx for Views/Package
    This is the index page that lists entire collections of packages belonging to user.
    
    Currently fetches all packages belonging to a user and displays the packages in a table.
    
    NOTE: Payload for Index.tsx in View/Packages can be reduced by querying only relevant information.

---
+++ src/Views/Package/Index.tsx
@@ -0,0 +1,99 @@
+import React, { PureComponent, ReactNode } from 'react'
+
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
+
+interface Props {
+    token?: string
+}
+
+// Resembles Package model
+export interface Package {
+    created_at: string
+    date: string
+    destination: string
+    id: number
+    location: string
+    reference_number: string
+    timeslot: string
+    updated_at: string
+    user_id: number
+}
+
+interface State {
+    token: string | undefined,
+    packages: Package[] | undefined
+}
+
+class PackageIndex extends PureComponent<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        console.log(this.props)
+        this.state = {
+            token: this.props.token,
+            packages: undefined
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/v1/get_packages', {
+            method: "GET",
+            headers: {
+                'Authorization': 'Bearer ' + this.state.token,
+            }
+        })
+            .then(res => res.json())
+            .then(json => {
+                let packages: Package[] = json
+                this.setState(() => ({
+                    packages: packages
+                }))
+            })
+    }
+
+    render(): ReactNode {
+        if (!this.state.packages) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <main>
+                    <h1>Package Index</h1>
+                    <button>New package</button>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Reference<div>Number</div></td>
+                                <td>Location</td>
+                                <td>Destination</td>
+                                <td>Timeslot</td>
+                                <td>Created On</td>
+                                <td>Last Update</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.packages.map(pkg => (
+                                <tr key={pkg.id}>
+                                    <td>{pkg.reference_number}</td>
+                                    <td>{pkg.location}</td>
+                                    <td>{pkg.destination}</td>
+                                    <td>{pkg.timeslot}</td>
+                                    <td>{pkg.created_at}</td>
+                                    <td>{pkg.updated_at}</td>
+
+                                    <Link to={`/view/${pkg.id}`}>view</Link>
+                                    <Link to={`/edit/${pkg.id}`}>edit</Link>
+                                    <Link to={`/delete/${pkg.id}`}>delete</Link>
+                                </tr>
+                            ))}
+
+                        </tbody>
+                    </table>
+                </main>
+            )
+        }
+    }
+}
+
+export default PackageIndex
