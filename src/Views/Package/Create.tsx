2c3861ccdb5dcb9aa715dbf7d4593c493e42af06
angeloreborn <angelowolffreborn@gmail.com>
2021-05-30 14:43:23 +0200

    Create Package/Create.tsx
    Create.tsx is used to create a new package.
    
    It renders a partial form '_Form', onFormSubmit passes an object that is constructed using the interface PackageForm in _Form.tsx
    
    Create contains a functional component Redirect.tsx, this component is used to redirect using React Hooks
    
    : Redirect is executed when this.state.redirect is set to contain the functional component.

---
+++ src/Views/Package/Create.tsx
@@ -0,0 +1,60 @@
+import { PureComponent, ReactNode } from 'react'
+import { PackageForm } from './_Form'
+import _Form from './_Form'
+import Redirect from '../../Components/Redirect'
+
+interface Props {
+    token?: string
+}
+interface State {
+    redirect: ReactNode
+}
+
+class Create extends PureComponent<Props, State> {
+    constructor(props: Props) {
+        super(props)
+
+        this.state = {
+            redirect: undefined
+        }
+        this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    }
+
+    // TODO handle req.status
+    handleFormSubmit(packageFormData: PackageForm) {
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + this.props.token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(packageFormData)
+        }
+
+        fetch('http://localhost:3000/api/v1/create_package', options)
+            .then(res => res.json())
+            .then(json => {
+                this.redirectToView()
+            })
+    }
+
+    redirectToView() {
+        this.setState(() => ({
+            redirect: <Redirect to={`/package/index`} />
+        }))
+
+    }
+
+    render(): ReactNode {
+        return (
+            <main>
+                <_Form onFormSubmit={this.handleFormSubmit}>
+                    <button type='submit'>Sumbit</button>
+                </_Form>
+                {this.state.redirect}
+            </main>
+        )
+    }
+}
+
+export default Create
